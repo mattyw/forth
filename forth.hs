907a17a78a52745a4a2fd9ec1272bd9780718618
mattyw <gh@mattyw.net>
2017-05-07 16:37:13 +0100

    forth.hs: Added sort of working parser

--- forth.hs
+++ forth.hs
@@ -3,39 +3,36 @@ module Main where
 -- TODO Maybe this should be a a Union Type
 -- with one option being [Int] -> [Int] and
 -- another being [Int] -> (Int, [Int]
-type StackOp = [Int] -> (Maybe Int, [Int])
-
-type OpWithResult = (Maybe Int, [Int])
-instance Monad OpWithResult where
-    op1 >>= op2 = \(_, stk) -> op2
-    return stk = \res -> (res, stk)
+type StackOp = [Int] -> [Int]
 
 push :: Int -> StackOp
-push n = \stk -> (Nothing, n : stk)
+push n = \stk -> n : stk
 
 add :: StackOp
 add [] = error "empty list"
 add [x] = error "not enough items"
-add (a:b:xs) = (Nothing, a+b : xs)
+add (a:b:xs) = a+b : xs
 
 mul :: StackOp
 mul [] = error "empty list"
 mul [x] = error "not enough items"
-mul (a:b:xs) = (Nothing, a*b : xs)
+mul (a:b:xs) = a*b : xs
 
+-- TODO decide how dot should work.
 dot :: StackOp
 dot [] = error "empty list"
-dot (x:xs) = (Just x, xs)
+dot (x:xs) = xs
 
-run :: [Int] -> [StackOp] -> (Maybe Int, [Int])
-run stk [] = (Nothing, stk)
+run :: [Int] -> [StackOp] -> [Int]
+run stk [] = stk
 run stk [op] = op stk
 run stk (op: ops) = run (op stk) ops
 
--- TODO maybe this isn't needed
-showResult :: (Maybe Int, [Int]) -> String
-showResult (Nothing, _) = ""
-showResult (Just x, _) = show x
+parse :: [String] -> [StackOp]
+parse [] = []
+parse ("*": xs) = mul : parse xs
+parse ("+": xs) = add : parse xs
+parse (x: xs) = push (read x :: Int) : parse xs
 
 main :: IO ()
-main = putStrLn $ show $ run [] [push 4, push 5]
+main = putStrLn $ show $ run [] $ parse $ words "5 6 + 7 8 + *"
